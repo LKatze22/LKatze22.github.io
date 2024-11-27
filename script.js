bbf3b38d8030b25421ded185cff93c3102e2ef1a
LKatze22 <164788766+LKatze22@users.noreply.github.com>
2024-11-27 19:05:22 +0100

    Add files via upload

--- script.js
+++
@@ -1,8 +0,0 @@
-function setbgw(){
-    document.body.style.backgroundColor=#FFFFFF;
-    document.body.style.color=#000000;
-}
-function setbgb(){
-    document.body.style.backgroundColor=#000000;
-    document.body.style.color=#FFFFFF;
-}
